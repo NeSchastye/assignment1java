9c0542a5bf6b861425027add639010c3ae172839
NeSchastye <vwolf9017@gmail.com>
2025-12-30 19:09:29 +0500

    +assignment 2 update

--- src/Artist.java
+++ src/Artist.java
@@ -1,9 +1,9 @@
-public class Artist {
-    private String name;
+import java.util.Objects;
+public class Artist extends MusicItem {
     private String country;
 
     public Artist(String name, String country) {
-        this.name = name;
+        super(name);
         this.country = country;
     }
 
@@ -26,5 +26,26 @@ public class Artist {
     @Override
     public String toString() {
         return "Artist{name='" + name + "', country='" + country + "'}";
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artist artist = (Artist) o;
+        return Objects.equals(name, artist.name) &&
+                Objects.equals(country, artist.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, country);
+    }
+    @Override
+    public String getDetails() {
+        return "Artist: " + name + " from " + country;
+    }
+    @Override
+    public void display() {
+        System.out.println(getDetails());
     }
 }
